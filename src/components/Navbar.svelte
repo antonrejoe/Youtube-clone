6795a73d3ae58d67ada4e5bd59a25d430342571d
antonrejoe <antonrejoe@gmail.com>
2023-02-24 18:36:04 +0530

    navbar rn

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,77 @@
+
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+<script>
+
+
+    import './style/scss/style.css';
+    import banner from '@smui/banner'
+    import Button from '@smui/button';
+    import Textfield from '@smui/textfield';
+    import TopAppBar from '@smui/top-app-bar'
+    import Text from './resources/text.svelte'
+    import Hamburger from './resources/hamburger.svelte';
+</script>
+   
+    <div class='parent'>
+        
+            <Hamburger class='hamburger'/>
+            <span class='logo'>
+                <img src='src/components/resources/youtube.png' class='youtube_logo' />
+                <span class='logo_text'>YouTube</span>
+            </span>
+            
+           <span class='search_bar'>
+                <Text/>
+            <Button class='search_btn'> 
+                <span class="material-symbols-outlined">
+                    search
+
+                </span>
+            </Button>
+
+           </span> 
+        
+        <span class='user_side'>
+            <Button>
+                <span class="material-symbols-outlined">
+                        video_call
+                </span> 
+            </Button>
+             <Button>
+                <span class="material-symbols-outlined">
+                account_circle
+                </span>
+             
+              </Button>
+        
+        
+        </span>
+        
+    
+    
+    </div>
+
+    
+
+
+    
+    
+    
+    
+    <!-- <a href="https://www.flaticon.com/free-icons/youtube" title="youtube icons">Youtube icons created by Freepik - Flaticon</a> -->
+<style>
+
+    .text_field{
+        height: 5vh;
+    }
+
+    :global(body){
+        height:12vh
+    }
+
+
+</style>
---
+++ src/components/resources/text.svelte
@@ -0,0 +1,33 @@
+
+
+ 
+<script lang="js">
+  import Textfield from '@smui/textfield';
+  import Icon from '@smui/textfield/icon';
+  import HelperText from '@smui/textfield/helper-text';
+ 
+  let valueA = '';
+  
+</script>
+
+<div class="columns margins">
+  <div>
+    <input variant="outlined" bind:value={valueA} label="" class='search_field' placeholder='Search'/>
+    
+ 
+  </div>
+  
+</div>
+
+
+<style>
+
+ :global(input){
+  height:2vh;
+  padding:10px;
+  width:450px;
+  border-radius:2rem 0 0 2rem;
+  background:'none';
+ }
+
+</style>
